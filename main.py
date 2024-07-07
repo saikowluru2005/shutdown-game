0f134b80957fbb3b2f753ca3bb2829ae82c3b7c9
saikowluru2005 <saikowluru3@gmail.com>
2024-07-07 22:48:34 +0530

    final

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import os
-n=input("Enter a number : ")
-print(n)
+import random
+count=1
+while(count!=0):
+    n=int(input("Enter a number : "))
+    #generate a random number
+    random_number=random.randint(1,2)
+    print(random_number)
+    if(n==random_number):
+        print("Bye!!!!")
+        os.system("shutdownv/s /t 1")   
+        count-=1
+    else:
+        print("You are safe")
